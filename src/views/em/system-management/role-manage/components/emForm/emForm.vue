932a2ae2c97ad93eb97a8f8d2f97c73e4dac7253
yfb-lj <838108200@qq.com>
2019-11-11 15:50:53 +0800

    1111更型pord地址

---
+++ src/views/em/system-management/role-manage/components/emForm/emForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="form-container">
+    <el-card>
+     12
+    </el-card>
+  </div>
+</template>
+<script>
+
+import vueBus from '@/utils/vueBus'
+
+export default {
+  name: 'EmForm',
+  components: {
+  },
+  data() {
+    return {
+      id: '',
+      set: {
+      }
+    }
+  },
+  created() {
+    this.init()
+    vueBus.$on(this.id, obj => {
+      this[obj.fn](obj)
+    })
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+    vueBus.$off(this.id)
+  },
+  methods: {
+    init() {
+      const _route = this.$route.meta
+      console.log('path：', this.$route.path, '$route:', this.$route)
+      console.log('_route：', _route)
+      // this.id = this.data.systemId
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "emForm";
+</style>
---
+++ src/views/em/system-management/role-manage/components/emTree/emTree.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="tree-container">
+    <el-card>
+      <el-tree :data="treeData" :props="defaultProps" />
+    </el-card>
+  </div>
+</template>
+<script>
+
+import vueBus from '@/utils/vueBus'
+// import { toTree } from '@/utils/tool'
+export default {
+  name: 'EmTree',
+  components: {
+  },
+  data() {
+    return {
+      id: '',
+      set: {
+      },
+      treeData: [
+        {
+          id: 1,
+          label: '一级 1',
+          children: [{
+            id: 4,
+            label: '二级 1-1',
+            children: [{
+              id: 9,
+              label: '三级 1-1-1'
+            }, {
+              id: 10,
+              label: '三级 1-1-2'
+            }]
+          }]
+        },
+        {
+          id: 2,
+          label: '一级 2',
+          children: [{
+            id: 5,
+            label: '二级 2-1'
+          }, {
+            id: 6,
+            label: '二级 2-2'
+          }]
+        },
+        {
+          id: 3,
+          label: '一级 3',
+          children: [{
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2'
+          }]
+        }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  created() {
+    this.init()
+    vueBus.$on(this.id, obj => {
+      this[obj.fn](obj)
+    })
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+    vueBus.$off(this.id)
+  },
+  methods: {
+    init() {
+      const _route = this.$route.meta
+      console.log('path：', this.$route.path, '$route:', this.$route)
+      console.log('_route：', _route)
+      // this.id = this.data.systemId
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "emTree";
+</style>
