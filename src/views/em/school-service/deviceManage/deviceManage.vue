9b8329702a2d4699a6ae7e9cbb748e6c2a3f9bdd
linda <linda@sina.com>
2019-12-02 15:17:27 +0800

    '选项卡'

--- src/views/em/school-service/deviceManage/deviceManage.vue
+++ src/views/em/school-service/deviceManage/deviceManage.vue
@@ -3,21 +3,21 @@
     <el-row style="height: 100%">
       <el-tabs v-model="tableTabsValue" :tab-position="tabPosition" @tab-click="handleClick">
         <el-tab-pane
+          v-for="item in meta.tableTabs"
           :ref="system_id"
-          v-for="(item, index) in meta.tableTabs"
-          :key="index"
+          :key="item.name"
           :label="item.text"
           :name="item.name"
         >
-            <div style="overflow: auto;height: 100%;">
-              <el-row>
-                <template v-for="(item,index) in children.paneC">
-                  <el-col :key="index" :offset="Number(item.meta.offset)" :span="Number(item.meta.span)">
-                    <component :is="item.meta.componentType" :key="index" :data="item" />
-                  </el-col>
-                </template>
-              </el-row>
-            </div>
+          <div style="overflow: auto;height: 100%;">
+            <el-row>
+              <template v-for="(item,index) in children.paneC">
+                <el-col :key="index" :offset="Number(item.meta.offset)" :span="Number(item.meta.span)">
+                <component :is="item.meta.componentType" v-if="item.meta.tableType===tableType" :key="index" :data="item"/>
+              </el-col>
+              </template>
+            </el-row>
+          </div>
         </el-tab-pane>
       </el-tabs>
     </el-row>
@@ -36,9 +36,10 @@ export default {
   mixins: [emMixin, emPage],
   data() {
     return {
+      tableType: 'device_manage',
       tableTabs: [], // 选项卡list
       tabPosition: 'left',
-      tableTabsValue: '',
+      tableTabsValue: 'device_manage',
       children: {
         paneC: []
       }
@@ -51,16 +52,18 @@ export default {
     init() {
       this.set = dataInitFn(this.set, this.meta)
       this.children = childrenInitFn(this.children, this.componentData)
-      console.log(2, this.children)
       for (const i in this.children) {
         this.children[i].forEach((val) => {
           // 拿到表格组id并赋值给当前显示的tabs
-          this.tableTabsValue = val.meta.system_id
         })
       }
     },
     handleClick(tab) {
-      console.log('name', tab.name)
+      if (tab.name === 'device_type_manage') {
+        this.tableType = 'device_type_manage'
+      } else if (tab.name === 'device_manage') {
+        this.tableType = 'device_manage'
+      }
     }
   }
 }
