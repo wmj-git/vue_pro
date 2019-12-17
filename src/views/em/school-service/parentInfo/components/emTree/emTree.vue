34a8f81fc3243832ed0e2bf2db0960c60fe66004
linda <linda@sina.com>
2019-12-17 16:08:08 +0800

    '班级信息获取'

--- src/views/em/school-service/parentInfo/components/emTree/emTree.vue
+++ src/views/em/school-service/parentInfo/components/emTree/emTree.vue
@@ -6,13 +6,17 @@
       :props="defaultProps"
       :load="loadNode"
       lazy
-      @node-click="handleNodeClick" />
+      @node-click="handleNodeClick"
+    />
   </div>
 </template>
 <script>
 import { fetchList } from '@/api/schoolService/classInfo'
+import { dataInitFn, childrenInitFn } from '@/utils/tool'
+import { emMixin } from '@/utils/mixins'
 export default {
   name: 'EmTree',
+  mixins: [emMixin],
   data() {
     return {
       treeData: [],
@@ -23,7 +27,8 @@ export default {
       },
       set: {
         queryUrl: ''
-      }
+      },
+      classData: [] // 班级
     }
   },
   created() {
@@ -31,31 +36,33 @@ export default {
   },
   methods: {
     init() {
-      fetchList({ // 班级信息
-        url: 'school/class/queryAllByPage'
-      }).then(response => {
-        console.log('班级信息', response.data.list)
-      })
+      this.set = dataInitFn(this.set, this.meta)
+      this.children = childrenInitFn(this.children, this.componentData)
     },
     handleNodeClick(data) {
       console.log(data)
     },
-    loadNode(node, resolve) {
+    async loadNode(node, resolve) {
       if (node.level === 0) {
         // 树根节点名称为当前登录学校名称（以中横线为分界取第一个字符串）
         const name = (this.$store.getters.currentRole.description).split('-')[0]
         return resolve([{ label: name }])
       }
-      if (node.level > 1) return resolve([])
-      setTimeout(() => {
-        const data = [{
-          label: 'leaf',
-          leaf: true
-        }, {
-          label: 'zone'
-        }]
-        resolve(data)
-      }, 500)
+      if (node.level === 2) return resolve([])
+      setTimeout(async() => {
+        var classArr = []
+        await fetchList({ // 班级信息
+          url: this.set.queryUrl
+        }).then(response => {
+          response.data.list.forEach(val => {
+            classArr.push({ 'label': val.name })
+          })
+        })
+        for (const i in classArr) {
+          this.classData.push({ label: classArr[i].label })
+          resolve(this.classData)
+        }
+      }, 300)
     }
   }
 }
