65ef7b67baec614b518b271098117ec35f5770f8
linda <linda@sina.com>
2019-12-27 11:24:45 +0800

    'dev'

--- src/views/em/school-service/tableInfo/components/emCropper/emCropper.vue
+++ src/views/em/school-service/tableInfo/components/emCropper/emCropper.vue
@@ -67,15 +67,15 @@ export default {
         outputType: 'jpeg', // 裁剪生成图片的格式
         canScale: false, // 图片是否允许滚轮缩放
         autoCrop: true, // 是否默认生成截图框
-        autoCropWidth: 300, // 默认生成截图框宽度
-        autoCropHeight: 200, // 默认生成截图框高度
+        autoCropWidth: '', // 默认生成截图框宽度
+        autoCropHeight: '', // 默认生成截图框高度
         fixedBox: true, // 固定截图框大小 不允许改变
         fixed: true, // 是否开启截图框宽高固定比例
         fixedNumber: [16, 9], // 截图框的宽高比例
         full: true, // 是否输出原图比例的截图
         canMoveBox: true, // 截图框能否拖动
         original: false, // 上传图片按照原始比例渲染
-        centerBox: false, // 截图框是否被限制在图片里面
+        centerBox: true, // 截图框是否被限制在图片里面
         infoTrue: true // true 为展示真实输出图片宽高 false 展示看到的截图框宽高
       },
       headers: {
@@ -107,7 +107,6 @@ export default {
       // 上传成功后将图片地址赋值给裁剪框显示图片
       this.$nextTick(() => {
         this.option.img = URL.createObjectURL(files.raw)
-        console.log(222, this.option.img)
         this.dialogVisible = true
       })
     },
