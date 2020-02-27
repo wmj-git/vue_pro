2cfd58d61dd05f71e2df08ce7de0f66a53a7db64
Linda <Linda@sina.com>
2020-02-27 13:48:40 +0800

    dev

--- src/components/NoticeMessage/index.vue
+++ src/components/NoticeMessage/index.vue
@@ -110,20 +110,20 @@ export default {
     }
   },
   created() {
-    this.init()
-    this.getList()
+    /* this.init()
+   this.getList()*/
     const token = this.$store.getters.token
     // ws：neinx那边加的，要是没有ws则会自动加上一个info,通道没法连接上
     // access_token: //后端的名字
     // sockets: 接口基本地址必须带的
-    const url = this.BASE_API + '/ws/sockets?access_token=' + token
+    /* const url = this.BASE_API + '/ws/sockets?access_token=' + token
     this.socketApi.initWebSocket(url)
     this.socketApi.proxyFunction(10, (res) => {
       if (res) {
         this.init() // 发送通知消息后继续获取数量
         this.getList()
       }
-    })
+    })*/
   },
   methods: {
     init() {
